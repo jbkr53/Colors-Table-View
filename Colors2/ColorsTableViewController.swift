8feba723eb79dfd8312fb296bb10df36a7b8e940
jbkr53 <you@yourdomain.comjbkr53@mail.umkc.edu>
2019-10-04 12:07:19 -0500

    first commit

---
+++ Colors2/ColorsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorsTableViewController.swift
+//  Colors2
+//
+//  Created by James Kirkland on 10/4/19.
+//  Copyright © 2019 James Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ColorsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
