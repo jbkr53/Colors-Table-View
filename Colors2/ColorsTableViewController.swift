ecfb337bbb57263bb1e51d82752e2fd9c9d6023e
jbkr53 <you@yourdomain.comjbkr53@mail.umkc.edu>
2019-10-04 12:22:22 -0500

    first commit

--- Colors2/ColorsTableViewController.swift
+++ Colors2/ColorsTableViewController.swift
@@ -8,15 +8,24 @@
 
 import UIKit
 
-class ColorsTableViewController: UIViewController {
-
+class ColorsTableViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    var Colors = ["red","orange","yellow","green","blue","purple","brown"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Colors.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        cell.textLabel?.text = Colors[indexPath.row]
+        return cell
+    }
     /*
     // MARK: - Navigation
 
