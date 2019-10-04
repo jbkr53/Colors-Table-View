d26f6bf52910906a7b04d58185ac481727bc43e0
jbkr53 <you@yourdomain.comjbkr53@mail.umkc.edu>
2019-10-04 11:58:09 -0500

    Initial Commit

---
+++ Colors2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors2
+//
+//  Created by James Kirkland on 10/4/19.
+//  Copyright © 2019 James Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
